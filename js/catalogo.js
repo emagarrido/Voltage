c5bf0b636dd96e8d4e280a8ef7c2b770beb79637
emagarrido <emagarrido@outlook.es>
2023-08-10 21:50:51 -0300

    arreglo el agregar a local storage

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,78 +1,3 @@
-// let txtRespuesta = document.querySelector("#txtRespuesta");
-
-// document.querySelector("#tipo").addEventListener("change", filtrarPorTipo)
-// document.querySelector("#txt_buscar").addEventListener("keyup", filtrarTePorNombre)
-
-
-// cargarTe(todos)
-
-// function cargarTe(_unArray) {
-
-//     let contenido = "";
-//     for (let i = 0; i <= _unArray.length - 1; i++) {
-//         contenido += `
-//               <article>
-//               <img src="img/te/${_unArray[i].img[0]}.jpg" alt="${_unArray[i].nombre}">
-//              <h1>${_unArray[i].nombre}</h1>
-//              <h2>${_unArray[i].ingredientes}</h2>
-//              <h3>Nivel de Teina: ${_unArray[i].nivelTeina}</h3>
-//              <a href="ampliacion.html?id=${_unArray[i].id}" >VER MÁS</a>
-//                 </article>`
-//     }
-
-//     document.querySelector("#catalogo").innerHTML = contenido
-// }
-
-// function filtrarPorTipo() {
-//     let filtradoPorTipo = [];
-//     let tipoSeleccionado = document.querySelector("#tipo").value;
-//     console.log(tipoSeleccionado);
-//     for (let i = 0; i <= todos.length - 1; i++) {
-//         if (todos[i].categoria == tipoSeleccionado || tipoSeleccionado == "todos") {
-//             filtradoPorTipo.push(todos[i])
-//         }
-//     }
-//     console.log(filtradoPorTipo);
-
-//     cargarTe(filtradoPorTipo)
-// }
-
-// function filtrarTePorNombre() {
-
-//     let txtRespuesta = ""
-
-//     let textoIngresado = document.querySelector("#txt_buscar").value;
-//     let teFiltradoPorNombre = [];
-//     for (let i = 0; i <= todos.length - 1; i++) {
-//         if (todos[i].nombre.toUpperCase().includes(textoIngresado.toUpperCase())) {
-//             teFiltradoPorNombre.push(todos[i])
-//         }
-//     }
-//     console.log(teFiltradoPorNombre);
-
-//     if (teFiltradoPorNombre.length == 0) {
-//         txtRespuesta.innerHTML += `<h3>No encontramos coincidencias con tu busqueda </h3>`
-//     }
-//     else {
-//         cargarTe(teFiltradoPorNombre)
-//     }
-
-// }
-
-// function modalOn() {
-//     modal.style.display = "block";
-// }
-
-// function modalOf() {
-//     modal.style.display = "none";
-// }
-// document.querySelector("#facts").addEventListener("click", modalOn)
-// document.querySelector("#cerrar").addEventListener("click", modalOf)
-
-
-
-
-
 let txtRespuesta = document.querySelector("#txtRespuesta");
 
 document.querySelector("#tipo").addEventListener("change", filtrarPorTipo);
@@ -80,11 +5,12 @@ document.querySelector("#txt_buscar").addEventListener("keyup", filtrarTePorNomb
 document.querySelector("#facts").addEventListener("click", modalOn);
 document.querySelector("#cerrar").addEventListener("click", modalOf);
 
-cargarTe(todos);
+cargarCatalogo(todos);
 
-function cargarTe(_unArray) {
+function cargarCatalogo(_unArray) {
     let contenido = "";
     for (let i = 0; i <= _unArray.length - 1; i++) {
+
         contenido += `
             <article>
                 <img src="img/te/${_unArray[i].img[0]}.jpg" alt="${_unArray[i].nombre}">
@@ -112,7 +38,7 @@ function filtrarPorTipo() {
         }
     }
     console.log(filtradoPorTipo);
-    cargarTe(filtradoPorTipo);
+    cargarCatalogo(filtradoPorTipo);
 }
 
 function filtrarTePorNombre() {
@@ -129,7 +55,7 @@ function filtrarTePorNombre() {
     } else {
         txtRespuesta.innerHTML = "";
     }
-    cargarTe(teFiltradoPorNombre);
+    cargarCatalogo(teFiltradoPorNombre);
 }
 
 function modalOn() {
@@ -143,22 +69,16 @@ function modalOf() {
 function agregarAlCarrito(event) {
     const idTe = event.target.getAttribute("data-id");
     let carrito = localStorage.getItem("carrito") ? JSON.parse(localStorage.getItem("carrito")) : [];
-    
+
     if (!carrito.some(item => item.id === idTe)) {
-        const teEncontrado = todos.find(te => te.id === idTe);
+        const teEncontrado = todos.find(te => parseInt(te.id) === parseInt(idTe));
         if (teEncontrado) {
             carrito.push(teEncontrado);
             localStorage.setItem("carrito", JSON.stringify(carrito));
             alert("Producto agregado al carrito");
+            console.log(carrito);
         }
     } else {
         alert("Este producto ya está en el carrito");
     }
 }
-
-function cargarCatalogo() {
-    const carrito = localStorage.getItem("carrito") ? JSON.parse(localStorage.getItem("carrito")) : [];
-    cargarTe(carrito);
-}
-
-cargarCatalogo();
